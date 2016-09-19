a633d2201a45a5f4c35f8d17d6479decb266c511
SikkandarSulaiman <sikkandarsulaiman@gmail.com>
2016-09-19 16:12:02 +0530

    Add files via upload

---
+++ out.php
@@ -0,0 +1,19 @@
+<?php
+system("gpio -g mode 2 out");
+system("gpio -g mode 3 out");
+system("gpio -g mode 4 out");
+system("gpio -g mode 7 out");
+system("gpio -g mode 8 out");
+system("gpio -g mode 9 out");
+system("gpio -g mode 10 out");
+system("gpio -g mode 11 out");
+system("gpio -g mode 14 out");
+system("gpio -g mode 15 out");
+system("gpio -g mode 17 out");
+system("gpio -g mode 18 out");
+system("gpio -g mode 22 out");
+system("gpio -g mode 23 out");
+system("gpio -g mode 24 out");
+system("gpio -g mode 25 out");
+system("gpio -g mode 27 out");
+?>
